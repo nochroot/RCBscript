53e9db7068801b93ac61de9361ff270ac233b1be
nochroot <59656972+nochroot@users.noreply.github.com>
2020-01-30 07:38:41 -0800

    minor. just added comments

--- RCB tool.user.js
+++ RCB tool.user.js
@@ -8,7 +8,33 @@
 // @grant        none
 // ==/UserScript==
 
-
+/*
+/////////////////////////
+/////////////////////////
+// Get readable values for easy pasting 
+// from md anderson RCB breast CA score 
+// http://www3.mdanderson.org/app/medcalc/index.cfm?pagename=jsconvert3
+//
+// 1/2020: as of now crudely just makes a button to press
+// i need to better understand javascript w some basic
+// youtube videso bc hitting another button after calculate is not ideal
+//
+// probably better to use an event listener for that button (eventlistener."click"?)?
+// or jquery or something else entirely 
+//
+//TODO
+//first thing is just work with the stupid button for now so can start getting 
+//readable text to put into real reports
+//this implementation (tampermonikey with button) probably not a long term solution
+//
+//need to understand their webpage and script
+//
+// alternatives is just make a website or webapp that puts in the values with no extra bullshit
+//and returns the RCB calculation and score and all elenemts in human readable format
+//should be ok as long as disclaimer stating this is experimental and 
+//to use the original calculator, which is propietary and can be found to public
+// on mdanderson's site. 
+*/
 
 (function() {
     'use strict'; // just leave this. Not worth understanding why
@@ -18,12 +44,13 @@
     //make a button so DOM(?) can load. otherwise the values we want are null
 var input=document.createElement("input");
 input.type="button";
-input.value="Custom Button to do shit~!~!~!~!";
+input.value="Custom (temporary ) button ... not the best way to do this";
 input.onclick = doWork;
+    
+//document.body.appendChild(input); 
+    //^Puts button way at bottom of page. Don't like...
 
-//document.body.appendChild(input); //Puts button way at bottom. Don't like...
-
-//this is ok button placement for now
+//this is better button placement for now
 var parentElement = document.getElementsByClassName("section-title")[0]; //where i want the button to be after. returns all elements (plural) as a "node list" so needs index
 parentElement.appendChild(input); //puts our button into the right spot
 
