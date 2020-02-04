0840034db655864f5d8e97ad552b0ba08c3fb2a5
nochroot <59656972+nochroot@users.noreply.github.com>
2020-02-03 20:17:15 -0800

    minor clean up. Works

--- RCB tool.user.js
+++ RCB tool.user.js
@@ -1,16 +1,18 @@
 // ==UserScript==
 // @name         RCB tool
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.0
 // @description  Grab RCB data elements
-// @author       SN
+// @author       ?
 // @match        http://www3.mdanderson.org/app/medcalc/index.cfm?pagename=jsconvert3
-// @grant        none
+// @grant GM_setClipboard
 // ==/UserScript==
 
 /*
 /////////////////////////
+The above metadata is needed for usersccript to work
 /////////////////////////
+//RCB TOOL
 // Get readable values for easy pasting
 // from md anderson RCB breast CA score
 // http://www3.mdanderson.org/app/medcalc/index.cfm?pagename=jsconvert3
@@ -34,41 +36,44 @@
 //should be ok as long as disclaimer stating this is experimental and
 //to use the original calculator, which is propietary and can be found to public
 // on mdanderson's site.
+
+//Notes on userscript metadata
+// @grant GM_setClipboard  makes clipboard ok per https://stackoverflow.com/questions/35576512/gm-setclipboard-and-other-gm-functions-gives-an-error-in-a-firefox-but-not-chr
+// GM setclpboard is a userscript/greese-monkey specific call to put stuff in clipboard. there are other ways of doing this
+
 */
 
 (function() {
-    'use strict'; // just leave this. Not worth understanding why
-
-
+    'use strict'; // recommended to just leave this. Not worth understanding why. Supposedly helps debugging.
 
     //make a button so DOM(?) can load. otherwise the values we want are null
-var customButton=document.createElement("input");
-customButton.type="button";
-customButton.value="COPY VALUES TO CLIPBOARD";
-customButton.onclick = doWork;
+    var customButton=document.createElement("input");
+    customButton.type="button";
+    customButton.value="COPY VALUES TO CLIPBOARD";
+    customButton.onclick = doWork;
 
-//document.body.appendChild(input);
+    //document.body.appendChild(input);
     //^Puts button way at bottom of page. Don't like...
 
-//this is better button placement for now
-var parentElement = document.getElementsByClassName("section-title")[0]; //where i want the button to be after. returns all elements (plural) as a "node list" so needs index
-parentElement.appendChild(customButton); //puts our button into the right spot
+    //the following is better button placement for now. Append it to the area before the table.
+    var parentElement = document.getElementsByClassName("section-title")[0]; //where i want the button to be after. returns all elements (plural) as a "node list" so needs index
+    parentElement.appendChild(customButton); //puts our button into the right spot
 
 
 
-function doWork()
+function doWork()     //grab the stuff and put into readable text
 {
-//grab the stuff and put into readable text
-//interestingly they dont use "id" (which are unique and able to use css) so cant use getElementById
-    //get elements by name returns a "node list" so you have to access by index
+    //interestingly they dont use "id" (id are unique and able to use css) so cant use getElementById
+    //instead, get elements by "name" returns a "node list" so you have to access by index...there is only one instance
+
     var d1 = document.getElementsByName("d1")[0].value;
     var d2 = document.getElementsByName("d2")[0].value;
-    var primaryTumorBedArea = "Primary tumor bed area: " + d1 + " mm X "  + d2 +  " mm ";
- //   console.log( 'Primary tumor bed area: '+ primaryTumorBedArea);
-   // alert('Primary tumor bed area: '+ primaryTumorBedArea);
+    var primaryTumorBedArea = "Primary tumor bed area: " + d1 + "mm X "  + d2 +  "mm ";
+    //   console.log( 'Primary tumor bed area: '+ primaryTumorBedArea);
+    // alert('Primary tumor bed area: '+ primaryTumorBedArea);
 
     var pCell = document.getElementsByName("pCell")[0].value;
-    var cellularity = "Overall Cancer Cellularity (as percentage of area):" + pCell + "%";
+    var cellularity = "Overall Cancer Cellularity (as percentage of area): " + pCell + "%";
 
     var pInSitu = document.getElementsByName("pInSitu")[0].value;
     var inSitu = "Percentage of cancer that is in situ disease: " + pInSitu + "%";
@@ -77,7 +82,7 @@ function doWork()
     var numberOfPositiveNodes = "Number of positive lymph nodes: " + nLn;
 
     var dMet = document.getElementsByName("dMet")[0].value;
-    var diameter = "Diameter of largest metastasis :" + dMet + " mm ";
+    var diameter = "Diameter of largest metastasis: " + dMet + "mm ";
 
     var rcbIndex = document.getElementsByName("rcbIndex")[0].value;
 
@@ -85,11 +90,12 @@ function doWork()
 
     var finalString = "Primary Tumor Bed \n" + primaryTumorBedArea + "\n" + cellularity + "\n" + inSitu + "\n"
                 + "\n"
-                + "Lymph nodes: " + "\n" + numberOfPositiveNodes + "\n" + diameter + "\n"
+                + "Lymph nodes " + "\n" + numberOfPositiveNodes + "\n" + diameter + "\n"
                 + "\n"
                 + "RCB index: " + rcbIndex + "\n"
                 + "RCB Class: " + rcbClass + "\n";
     alert ( "copy to clipboard" + finalString);
+    GM_setClipboard (finalString); //set clipboard (https://sourceforge.net/p/greasemonkey/wiki/GM_setClipboard/)
 
 }
 })(
